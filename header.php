ad7483597f15bad9a54ab099255af1658a900851
Nate Druss <nate@nsideas.com>
2018-04-14 13:32:01 -0400

    Initialized project

---
+++ header.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Graduation Countdown</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        
+        <link href="https://fonts.googleapis.com/css?family=Karla:400,700" rel="stylesheet">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body class="bg_surf">
--- index.php
+++ index.php
@@ -1,37 +1,16 @@
-<!doctype html>
-<html class="no-js" lang="">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Graduation Countdown</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
-        <link rel="stylesheet" href="css/grid.css">
-        <link href="https://fonts.googleapis.com/css?family=Karla:400,700" rel="stylesheet">
-        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
-    </head>
-    <body class="bg_surf">
+<?php require 'header.php' ?>
 
-        <div class="wrapper">
+    <div class="wrapper">
 
-            <?php
-            $d1=strtotime("December 17");
-            $d2=ceil(($d1-time())/60/60/24);
-            ?>
+        <?php
+        $d1=strtotime("December 17, 2016");
+        $d2=ceil(($d1-time())/60/60/24);
+        ?>
 
-            <p>Days 'til graduation:</p>
+        <p>Days 'til graduation:</p>
+        <h1 class="countdown"><?php echo $d2; ?></h1>
 
-            <h1 class="countdown"><?php echo $d2; ?></h1>
+    </div>
 
-        </div>
-
-        <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
-
-    </body>
-</html>
+<?php require 'footer.php' ?>
